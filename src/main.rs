d47e0ff32e38b6ebb42c23ae5e423ee27125182b
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2019-11-09 23:50:25 +0100

    winit -> iced::platform

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,24 @@
 #!/usr/bin/env -S cargo run
-use iced::winit as platform;
+
 struct Notifications {
     notification : notify_rust::Notification
 }
+
 #[derive(Debug)]
 pub enum Message {}
+
 impl iced::Application for Notifications {
-    fn title(&self) -> std::string::String { self.notification.summary.clone() }
     type Message = Message;
+
+    fn title(&self) -> std::string::String {
+        self.notification.summary.clone()
+    }
+
     fn update(&mut self, message: Message) {
         match message {
         }
     }
+
     fn view(&mut self) -> iced::Element<Message> {
         let content = iced::Text::new(&self.notification.body);
         iced::Column::new()
@@ -30,10 +37,11 @@ fn main() {
     notify_rust::server::NotificationServer::start(&notify_rust::server::NotificationServer::create(), |notification| {
         println!("{:#?}", notification);
 
-        let event_loop = platform::event_loop::EventLoop::new();
-        let window = platform::window::Window::new(&event_loop).unwrap(); // FIXME: do not show. only to get on which current_monitor WM would map
+        let event_loop = iced::platform::EventLoop::new();
+        let window = iced::platform::Window::new(&event_loop).unwrap(); // FIXME: do not show. only to get on which current_monitor WM would map
         let size = window.current_monitor().size();
-        window.set_inner_size(platform::dpi::LogicalSize::from_physical(platform::dpi::PhysicalSize{width: size.width/2., height: size.height/2.}, window.hidpi_factor()));
-        iced::platform::Application::run(iced::Instance(Notifications{notification:notification.clone()}), event_loop, window);
+        window.set_inner_size(iced::platform::dpi::LogicalSize::from_physical(iced::platform::dpi::PhysicalSize{width: size.width/2., height: size.height/2.}, window.hidpi_factor()));
+        use iced::Application;
+        Notifications{notification:notification.clone()}.run(event_loop, window);
     });
 }
