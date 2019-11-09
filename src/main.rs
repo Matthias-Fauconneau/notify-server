6f41fe0c77c22ea4e8624f8475b28d873aa7714e
Matthias Fauconneau <matthias.fauconneau@gmail.com>
2019-11-09 23:14:07 +0100

    Set window size (and title) [local iced]

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #!/usr/bin/env -S cargo run
-use iced::{Application};
+use iced::winit as winit;
 struct Notifications {
     notification : notify_rust::Notification
 }
 #[derive(Debug)]
 pub enum Message {}
 impl iced::Application for Notifications {
+    fn title(&self) -> std::string::String { self.notification.summary.clone() }
     type Message = Message;
     fn update(&mut self, message: Message) {
         match message {
@@ -28,6 +29,11 @@ fn main() {
     });
     notify_rust::server::NotificationServer::start(&notify_rust::server::NotificationServer::create(), |notification| {
         println!("{:#?}", notification);
-        Notifications{notification:notification.clone()}.run();
+
+        let event_loop = winit::event_loop::EventLoop::new();
+        let window = winit::window::Window::new(&event_loop).unwrap(); // FIXME: do not show. only to get on which current_monitor WM would map
+        let size = window.current_monitor().size();
+        window.set_inner_size(winit::dpi::LogicalSize::from_physical(winit::dpi::PhysicalSize{width: size.width/2., height: size.height/2.}, window.hidpi_factor()));
+        iced::platform::Application::run_winit(iced::Instance(Notifications{notification:notification.clone()}), event_loop, window);
     });
 }
